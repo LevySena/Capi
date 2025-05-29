93bf0716c886dc6bf3775ce47f9b9ce0929a645a
LevySena <marcoslevysena10@gmail.com>
2025-05-29 08:39:52 -0300

    Implementando tela de login

--- calcarb/views.py
+++ calcarb/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render,redirect
 # Create your views here.
 
 def CalP(request):
-    return render(request,"calc/index.html")
+    return render(request,"calc/calcP.html")
