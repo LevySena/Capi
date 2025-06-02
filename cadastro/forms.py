c81030e7d91f8fc66d3f2c4259f0f76d0a5be9f2
LevySena <marcoslevysena10@gmail.com>
2025-06-02 15:46:15 -0300

    Criação de tabela e rota de cadastro em progresso

--- cadastro/forms.py
+++ cadastro/forms.py
@@ -2,13 +2,12 @@ from django import forms
 from django.contrib.auth.models import User
 
 class Cadastro_Form(forms.ModelForm):
-    cpf = forms.CharField(max_length=11,widget=forms.TextInput(attrs={'placeholde':'Cpf'}))
+    cpf = forms.CharField(max_length=11,widget=forms.TextInput(attrs={'placeholder':'Cpf','class':'form-control'}))
     class Meta:
         model = User
         fields = ['username','email','password']
         widgets = {
-            'username' : forms.TextInput(attrs={'placeholder':'Nome do usuário'}),
-            'email' : forms.EmailInput(attrs={'placeholder':'Nome do usuário'}),
-            'password' : forms.PasswordInput(attrs={'placeholder':'Nome do usuário'}),
-            
+            'username' : forms.TextInput(attrs={'placeholder':'Nome do usuário','class':'form-control'}),
+            'email' : forms.EmailInput(attrs={'placeholder':'Email','class':'form-control'}),
+            'password' : forms.PasswordInput(attrs={'placeholder':'Senha','class':'form-control'}),
         }
--- cadastro/views.py
+++ cadastro/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpRequest
+from cadastro.models import Cadastro_Pessoa
+from cadastro.forms import Cadastro_Form
+from django.contrib.auth.hashers import make_password
 
 # Create your views here.
-def Cad(request):
-    return render(request,'cad/tela-cad.html')
+def Cad(request: HttpRequest):
+    formulario = Cadastro_Form()
+    contexto={
+        'form' : formulario
+    }
+    if request.method == 'POST':
+        formulario = Cadastro_Form(request.POST)
+        if formulario.is_valid():
+            novo = formulario.save()
+            senha_cryp = make_password(request.POST['password'])
+            novo.password = senha_cryp
+            novo.save()
+            perfil = Cadastro_Pessoa()
+            perfil.cpf = formulario.cleaned_data['cpf']
+            perfil.user = novo
+            perfil.save()
+            #Redirecionar quando tela de login estiver pronta
+    return render(request,'cad/tela-cad.html',context=contexto)
