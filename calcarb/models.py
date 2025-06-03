51eafeab7a1af8b4974e7a283b3613d05732a882
LevySena <marcoslevysena10@gmail.com>
2025-06-03 12:53:22 -0300

    Criando tabelas de mensagem e calculadora

--- calcarb/models.py
+++ calcarb/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class PegadaCarb(models.Model):
+    usuario = models.ForeignKey(User,on_delete=models.CASCADE,related_name="PegadaCarbono")
+    dataRe = models.DateField()
+    valorTotal = models.IntegerField(blank=True,null=True)
+
+class Mensagem(models.Model):
+    cont = models.TextField()
+    positiva = models.BooleanField(default=True)
+    
