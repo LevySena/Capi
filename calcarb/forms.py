ecd138150540408c48a1dded76f469dbdc420e28
LevySena <marcoslevysena10@gmail.com>
2025-06-09 15:20:31 -0300

    Finalizando calculadora de carbono

--- calcarb/forms.py
+++ calcarb/forms.py
@@ -11,22 +11,22 @@ TRANSPORTE_VEIC = {
 }
 TIPO_COMBUST_CAR = {
     "-" : "Selecione o combustível",
-    "disel" : "Diesel",
-    "etanol" : "Etanol",
-    "gasolina" : "Gasolina",
-    "gnv" : "GNV",
+    "diesel" : "Diesel", #12,3 km/l
+    "etanol" : "Etanol", #11,4 km/l
+    "gasolina" : "Gasolina", #17,5 km/l
+    "gnv" : "GNV", #16 km/m3
     #"elec" : "Elétrico"
 }
 TIPO_COMBUST_MOTO = {
     "-" : "Selecione o combustível",
-    "etanol" : "Etanol",
-    "gasolina" : "Gasolina",
+    "etanol" : "Etanol", #24,7 km/l
+    "gasolina" : "Gasolina", #35 km/l
 }
 
 def veic_validator(valor):
     if valor == "-":
         raise ValidationError("Selecione um valor")
-
+    
 
 class Perguntas(forms.Form):
     precMes = forms.FloatField(widget=forms.NumberInput(attrs={'placeholder':'Responda em R$','class':'form-control'})) # x 0,0385 mensal
--- calcarb/views.py
+++ calcarb/views.py
@@ -12,18 +12,38 @@ def CalP(request : HttpRequest):
 
 def CalQ(request : HttpRequest):
     formulario = Perguntas()
-    contexto = {
-        "forms" : formulario
-    }
     if request.method == 'POST':
         formulario = Perguntas(request.POST)
         # for k,c in request.POST.items():
         #     print(f'{k} = {c}')
         if formulario.is_valid():
             print("Valido")
-            carbElec = (formulario.cleaned_data['precMes']/formulario.cleaned_data['precRegion']) * 0.0385
-            carbGas = formulario.cleaned_data['gasCoz'] * 2,9380
-            
+            carbElec = None
+            try:
+                carbElec = (formulario.cleaned_data['precMes']/formulario.cleaned_data['precRegion']) * 0.0385
+                carbGas = formulario.cleaned_data['gasCoz'] * 2.9380
+                if formulario.cleaned_data['veiculoP'] == "carro":
+                    if formulario.cleaned_data['tCombC'] == "diesel":
+                        carbLKm = (formulario.cleaned_data['krodado'] / 12.3)*2.44
+                    elif formulario.cleaned_data['tCombC'] == "etanol":
+                        carbLKm = (formulario.cleaned_data['krodado']/11.4)*1.50
+                    elif formulario.cleaned_data['tCombC'] == "gasolina":
+                        carbLKm = (formulario.cleaned_data['krodado']/17.5)*2.19
+                    else:
+                        carbLKm = (formulario.cleaned_data['krodado']/16)*1.92
+                elif formulario.cleaned_data['veiculoP'] == "moto":
+                    if formulario.cleaned_data['tCombC'] == "etanol":
+                        carbLKm = (formulario.cleaned_data['krodado']/24.7)*1.50
+                    else:
+                        carbLKm = (formulario.cleaned_data['krodado']/35)*2.19
+                else:
+                    carbLKm = 0
+                carbTotal = carbLKm + carbGas + carbElec
+                print(f"Carbono total = {carbTotal}")
+            except ZeroDivisionError:
+                formulario.add_error('precRegion','Digite um valor maior que zero')
         else: print(formulario.errors)
-
+    contexto = {
+    "forms" : formulario
+    }
     return render(request,"calc/calcQ.html",context=contexto)
